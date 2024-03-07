901a2a5eccbe4278a2ce7a4b07ee7ffd5926e736
AMAAN SARFARAZ <sarfarazmohammad369@gmail.com>
2024-03-07 16:48:48 +0530

    setting up appwrite and the app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import conf from "./conf/conf.js"
+function App() {
+  console.log(conf.appWriteUrl);
+  return (
+    <>
+      <h1>blog app in appwrite</h1>
+    </>
+  )
+}
+
+export default App
